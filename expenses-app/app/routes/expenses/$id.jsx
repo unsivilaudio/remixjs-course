28968357044383ab80f17b47ee8392fead6e85fa
unsivil <unsivilaudio@gmail.com>
2022-11-10 09:28:25 -0700

    scaffold out new expenses proj with routes

---
+++ expenses-app/app/routes/expenses/$id.jsx
@@ -0,0 +1,3 @@
+export default function ExpenseDetailPage() {
+    return <h1>Expense detail page</h1>;
+}
---
+++ expenses-app/app/routes/expenses/analysis.jsx
@@ -0,0 +1,3 @@
+export default function ExpensesAnalysisPAge() {
+    return <h1>Expenses Analysis Page</h1>;
+}
