d6ec1e4af0872b069b3f1e0ccb1483499366c538
unsivil <unsivilaudio@gmail.com>
2022-11-11 12:19:41 -0700

    wrap up section 4

--- expenses-app/app/routes/__app/expenses.analysis.jsx
+++ expenses-app/app/routes/__app/expenses.analysis.jsx
@@ -1,6 +1,8 @@
-import { useLoaderData } from 'public/build/_shared/chunk-NA56Z7W5';
+import { json } from '@remix-run/node';
+import { useCatch, useLoaderData } from '@remix-run/react';
 import Chart from '~/components/expenses/Chart';
 import ExpenseStatistics from '~/components/expenses/ExpenseStatistics';
+import Error from '~/components/util/Error';
 import { getExpenses } from '~/data/expenses.server';
 
 export default function ExpensesAnalysisPage() {
@@ -17,5 +19,29 @@ export default function ExpensesAnalysisPage() {
 }
 
 export async function loader() {
-    return getExpenses();
+    const expenses = await getExpenses();
+
+    if (!expenses?.length) {
+        throw json(
+            { message: 'Could not load expenses fro the requested analysis.' },
+            {
+                status: 404,
+                statusText: 'Not Found',
+            }
+        );
+    }
+
+    return expenses;
+}
+
+export function CatchBoundary() {
+    const caughtResponse = useCatch();
+
+    return (
+        <main>
+            <Error title={caughtResponse.statusText}>
+                <p>{caughtResponse.data?.message || 'Something went wrong.'}</p>
+            </Error>
+        </main>
+    );
 }
